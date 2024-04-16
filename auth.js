a508e4545d44f1a82cb41eba7fc8fb1cb5149980
swish <vansh.2210@outlook.com>
2024-04-16 16:16:00 +0530

    set expire null jwt

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ const verifyJWT = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, 'secret123');
+    const decoded = jwt.verify(token, "secret123");
     // req.user = decoded; // Attach decoded user information to the request object
     next();
   } catch (error) {
@@ -19,12 +19,15 @@ const verifyJWT = (req, res, next) => {
   }
 };
 
-const generateJWT=(payload, expiresIn = "1h")=> {
+const generateJWT = (payload, expiresIn = null) => {
   const options = {
     expiresIn,
   };
-  const secretKey = 'secret123'; // Assuming you have loaded the secret key from an environment variable
+  if (expiresIn !== null) {
+    options.expiresIn = expiresIn;
+  }
+  const secretKey = "secret123"; // Assuming you have loaded the secret key from an environment variable
   return jwt.sign(payload, secretKey, options);
-}
+};
 
-module.exports = {verifyJWT, generateJWT};
+module.exports = { verifyJWT, generateJWT };
